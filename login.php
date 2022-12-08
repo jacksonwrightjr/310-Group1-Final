5acb6bbb81ef92fe541d8bf32d2d1db4a4159587
shanebrown013 <shanebrown013@gmail.com>
2022-12-08 15:03:05 -0600

    commit

--- login.php
+++ login.php
@@ -1,21 +1,5 @@
 <html>
     <head>
-<<<<<<< HEAD
-        <title>My first PHP Website</title>
-    </head>
-    <body>
-        <h2>Login Page</h2>
-        <a href="index.php">Click here to go back</a>
-        <form action="checklogin.php" method="POST">
-           Enter Username: <input type="text" 
-           name="username" required="required" /> <br/>
-           Enter password: <input type="password" 
-           name="password" required="required" /> <br/>
-           <input type="submit" value="Login"/>
-        </form>
-    </body>
-</html>
-=======
         <title>Login</title>
         <link rel = "stylesheet" href = "style.css">
         <link rel= "icon" type = "image/x-icon" href = "img/tooth.png">
@@ -44,4 +28,3 @@
         </div>
     </body>
 </html>
->>>>>>> 8414af2 (front end additions to index and sign in)
